8c48fb177ce7ed5be512987852382a6bf37ec956
허수호 <dev.sooho@gmail.com>
2024-03-22 16:18:20 +0900

    5.4

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -2,14 +2,25 @@ import './App.css'
 import Header from "./components/Header.jsx";
 import Main from "./components/Main.jsx";
 import Footer from "./components/Footer.jsx";
+import Button from "./components/Button.jsx";
 
 function App() {
 
+    const buttonProps = {
+        text: "메일",
+        color: "red",
+        a: 1,
+        b: 2,
+        c: 3
+    };
+
   return (
     <>
-        <Header></Header>
-        <Main></Main>
-        <Footer></Footer>
+        <Button {...buttonProps}></Button>
+        <Button text={"카페"}></Button>
+        <Button text={"블로그"}>
+            <Header />
+        </Button>
     </>
   )
 }
---
+++ section05/src/components/Button.jsx
@@ -0,0 +1,15 @@
+const Button = ({text, color, children}) => {
+
+    return (
+        <button style={{color: color }}>
+            {text} - {color.toUpperCase()}
+            {children}
+        </button>
+    )
+}
+
+Button.defaultProps = {
+  color: "black",
+};
+
+export default Button;
