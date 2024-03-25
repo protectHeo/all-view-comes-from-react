250a11607d53e4e0e15b647b1f239bb97db617d8
허수호 <dev.sooho@gmail.com>
2024-03-25 22:56:10 +0900

    5.6

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,26 +1,31 @@
 import './App.css'
-import Header from "./components/Header.jsx";
-import Main from "./components/Main.jsx";
-import Footer from "./components/Footer.jsx";
-import Button from "./components/Button.jsx";
+import {useState} from "react";
 
 function App() {
+    const [count, setCount] = useState(0);
+    const [light, setLight] = useState("OFF");
 
-    const buttonProps = {
-        text: "메일",
-        color: "red",
-        a: 1,
-        b: 2,
-        c: 3
-    };
 
   return (
     <>
-        <Button {...buttonProps}></Button>
-        <Button text={"카페"}></Button>
-        <Button text={"블로그"}>
-            <Header />
-        </Button>
+        <div>
+            <h1>{light}</h1>
+            <button onClick={
+                () => {
+                    setLight(light === "ON" ? "OFF" : "ON");
+                }
+            }>{light === "ON" ? "끄기" : "켜기"}</button>
+        </div>
+        <div>
+            <h1>{count}</h1>
+            <button onClick={() => {
+                setCount(count + 1);
+            }}
+            >
+                +
+            </button>
+        </div>
+
     </>
   )
 }
