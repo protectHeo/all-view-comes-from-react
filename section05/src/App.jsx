5de6b17b693b60f519e1ff516a717e6e191bdb27
허수호 <dev.sooho@gmail.com>
2024-03-26 22:43:37 +0900

    5.8

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
-import Bulb from "./components/Bulb.jsx";
-import Counter from "./components/Counter.jsx";
+import Register from "./components/Register.jsx";
 
 function App() {
     return (
         <>
-            <Bulb></Bulb>
-            <Counter></Counter>
+            <Register></Register>
         </>
     );
 }
