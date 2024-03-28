140e210ee168a1f1363eab840757afbf8700c998
허수호 <dev.sooho@gmail.com>
2024-03-28 21:59:14 +0900

    6.3

--- section06/src/App.jsx
+++ section06/src/App.jsx
@@ -1,18 +1,23 @@
 import './App.css'
 import Viewer from "./components/Viewer.jsx";
 import Controller from "./components/Controller.jsx";
+import {useState} from "react";
 
 function App() {
+    const [count, setCount] = useState(0);
 
+    const onClickButton = (value) => {
+        setCount(count + value);
+    }
 
   return (
     <div className="App">
         <h1>Simple Counter</h1>
         <section>
-            <Viewer></Viewer>
+            <Viewer count={count}></Viewer>
         </section>
         <section>
-            <Controller></Controller>
+            <Controller onClickButton={onClickButton}></Controller>
         </section>
     </div>
   )
