c20de54ebdb68314e4ea497d3a28d954f8abb790
허수호 <dev.sooho@gmail.com>
2024-03-28 17:42:51 +0900

    6.2

--- section06/src/App.jsx
+++ section06/src/App.jsx
@@ -1,12 +1,20 @@
 import './App.css'
+import Viewer from "./components/Viewer.jsx";
+import Controller from "./components/Controller.jsx";
 
 function App() {
 
 
   return (
-    <>
-
-    </>
+    <div className="App">
+        <h1>Simple Counter</h1>
+        <section>
+            <Viewer></Viewer>
+        </section>
+        <section>
+            <Controller></Controller>
+        </section>
+    </div>
   )
 }
 
---
+++ section06/src/components/Viewer.jsx
@@ -0,0 +1,9 @@
+
+const Viewer = () => {
+    return <>
+        <div>현재 카운트 : </div>
+        <h1>0</h1>
+    </>;
+}
+
+export default Viewer;
