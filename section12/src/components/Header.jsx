3499d697e63b6af33ab86509d81177bf928ba361
허수호 <dev.sooho@gmail.com>
2024-04-17 01:05:10 +0900

    12.7

--- section12/src/components/Header.jsx
+++ section12/src/components/Header.jsx
@@ -1,10 +1,10 @@
-import "Header.css"
+import "./Header.css"
 
 const Header = ({title, leftChild, rightChild}) => {
     return <header className="Header">
-        <div className="header_left"></div>
-        <div className="header_center"></div>
-        <div className="header_right"></div>
+        <div className="header_left">{leftChild}</div>
+        <div className="header_center">{title}</div>
+        <div className="header_right">{rightChild}</div>
     </header>
 }
 
