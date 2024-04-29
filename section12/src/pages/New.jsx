ffb6dcbb4e09587d8774cde3b98ab05e98faf28a
허수호 <dev.sooho@gmail.com>
2024-04-30 06:06:17 +0900

    12.12

--- section12/src/pages/New.jsx
+++ section12/src/pages/New.jsx
@@ -1,7 +1,17 @@
-
+import Header from "../components/Header.jsx";
+import Button from "../components/Button.jsx";
+import Editor from "../components/Editor.jsx";
 
 const New = () => {
-    return <></>
+    return (
+        <div>
+            <Header
+                title={"새 일기 쓰기"}
+                leftChild={<Button text={"< 뒤로 가기"}/>}
+            />
+            <Editor />
+        </div>
+    );
 };
 
 export default New;
