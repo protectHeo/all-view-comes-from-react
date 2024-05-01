6907fe046b1afef3bf07229dd9ad2c2f75c8693c
허수호 <dev.sooho@gmail.com>
2024-05-01 23:08:18 +0900

    12.14

--- section12/src/components/EmotionItem.jsx
+++ section12/src/components/EmotionItem.jsx
@@ -2,10 +2,15 @@ import "./EmotionItem.css"
 import {getEmotionImage} from "../util/get-emotion-image.js";
 
 // eslint-disable-next-line react/prop-types
-const EmotionItem = ({emotionId, emotionName, isSelected}) => {
-    return <div className={`EmotionItem ${isSelected ? `EmotionItem_on_${emotionId}` : ``}`}>
-        <img className="emotion_img" src={getEmotionImage(emotionId)}/>
-        <div className="emotion_name">{emotionName}</div>
+const EmotionItem = ({emotionId, emotionName, isSelected, onClick}) => {
+    return
+    // eslint-disable-next-line no-unreachable
+    <div
+        onClick={onClick}
+        className={`EmotionItem ${isSelected ? `EmotionItem_on_${emotionId}` : ""}`}
+    >
+            <img className="emotion_img" src={getEmotionImage(emotionId)}/>
+            <div className="emotion_name">{emotionName}</div>
     </div>;
 }
 
--- section12/src/pages/New.jsx
+++ section12/src/pages/New.jsx
@@ -1,15 +1,31 @@
 import Header from "../components/Header.jsx";
 import Button from "../components/Button.jsx";
 import Editor from "../components/Editor.jsx";
+import {useNavigate} from "react-router-dom";
+import {useContext} from "react";
+import {DiaryDispatchContext} from "../App.jsx";
 
 const New = () => {
+    const {onCreate} = useContext(DiaryDispatchContext);
+    const nav = useNavigate();
+
+    const onSubmit = (input) => {
+        onCreate(input.createdDate.getTime(), input.emotionId, input.content);
+        nav("/", {replace: true});
+    };
+
     return (
         <div>
             <Header
                 title={"새 일기 쓰기"}
-                leftChild={<Button text={"< 뒤로 가기"}/>}
+                leftChild={
+                    <Button
+                        text={"< 뒤로 가기"}
+                        onClick={()=> nav(-1)}
+                    />
+                }
             />
-            <Editor />
+            <Editor onSubmit={onSubmit}/>
         </div>
     );
 };
